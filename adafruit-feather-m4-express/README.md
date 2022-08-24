8af1a3ccf6444f026427541582a259b05d8af08f
chris-albert <creasetoph@gmail.com>
2022-08-23 21:26:50 -0700

    getting m4 to actaully be a usb midi device ...

---
+++ adafruit-feather-m4-express/README.md
@@ -0,0 +1,3 @@
+
+ - [platformio](https://docs.platformio.org/en/latest/boards/atmelsam/adafruit_feather_m4.html)
+ - [MIDI over USB](https://tttapa.github.io/Control-Surface-doc/Doxygen/d8/d4a/md_pages_MIDI-over-USB.html)
---
+++ adafruit-feather-m4-express/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include "MIDIUSB.h"
+// #include <Control_Surface.h>
+
+// Instantiate a MIDI over USB interface.
+// USBMIDI_Interface midi;
+
+int LED = LED_BUILTIN;
+String msg = "";
+
+void setup() {
+
+    Serial.begin(31250);
+    // Control_Surface.begin();
+}
+
+void loop() {
+  midiEventPacket_t rx;
+  do {
+    rx = MidiUSB.read();
+    if (rx.header != 0) {
+      Serial.print("Received: ");
+      Serial.print(rx.header, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte1, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte2, HEX);
+      Serial.print("-");
+      Serial.println(rx.byte3, HEX);
+    }
+  } while (rx.header != 0);
+}
