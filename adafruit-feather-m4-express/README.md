bd2b1688c6e7308c9e6e6ad8e4f8c06f4af3167a
chris-albert <creasetoph@gmail.com>
2022-08-25 16:02:00 -0700

    Getting midi status neopixel to work

--- adafruit-feather-m4-express/README.md
+++ adafruit-feather-m4-express/README.md
@@ -13,7 +13,7 @@ pio run --target upload --environment usbmidi
 ```
 pio run -t clean
 ```
-
+ - [m4 pinout](https://github.com/adafruit/Adafruit-Feather-M4-Express-PCB/blob/master/Adafruit%20Feather%20M4%20Express%20Pinout.pdf)
  - [platformio](https://docs.platformio.org/en/latest/boards/atmelsam/adafruit_feather_m4.html)
  - [MIDI over USB](https://tttapa.github.io/Control-Surface-doc/Doxygen/d8/d4a/md_pages_MIDI-over-USB.html)
  - [MIDIUSB](https://www.arduino.cc/reference/en/libraries/midiusb/)
--- adafruit-feather-m4-express/src/main-usbmidi.cpp
+++ adafruit-feather-m4-express/src/main-usbmidi.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
 #include <USB-MIDI.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN       8
+#define NUMPIXELS 1
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 USBMIDI_CREATE_DEFAULT_INSTANCE();
+
+void noteOnStatus() {
+    pixels.setPixelColor(0, pixels.Color(0, 150, 0));
+    pixels.show();
+    delay(100);
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
+}
 
 String noteOnMessage = "";
 void handleNoteOn(byte channel, byte pitch, byte velocity) {
     noteOnMessage = "Midi Note on - ";
     noteOnMessage.concat(pitch);
     Serial.println(noteOnMessage);
+    noteOnStatus();
 }
 
 void setup() {
-    MIDI.begin(1);
+    MIDI.begin(MIDI_CHANNEL_OMNI);
     MIDI.setHandleNoteOn(handleNoteOn);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    pixels.begin();
 }
 
 void loop() {
